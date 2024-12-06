98dc3f7b8f9aebdc0b7ae84a5b19429916f88661
Tainix <bonjour@tainix.fr>
2024-12-07 00:05:50 +0100

    Day 3 - Part 2

--- day_3.php
+++ day_3.php
@@ -11,8 +11,12 @@ foreach ($data as $values) {
     $instructions .= $values;
 }
 
-// Expression régulière qui récupère dans $data les mul(x,y) ou x et y sont des entiers
-$pattern = '/mul\((\d+),(\d+)\)/';
+// Expression régulière qui récupère dans $data les séquences :
+// - mul(x,y) ou x et y sont des entiers
+// - do()
+// - don't()
+$pattern = '/mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)/';
+
 $calculs = [];
 preg_match_all($pattern, $instructions, $calculs);
 
@@ -21,8 +25,24 @@ function mul(int $x, int $y): int
     return $x * $y;
 }
 
+$activate = true;
 $sum = 0;
 foreach ($calculs[0] as $calcul) {
+
+    if ($calcul === 'do()') {
+        $activate = true;
+        continue;
+    }
+
+    if ($calcul === 'don\'t()') {
+        $activate = false;
+        continue;
+    }
+
+    if (! $activate) {
+        continue;
+    }
+
     $sum += eval('return ' . $calcul . ';');
 }
 
